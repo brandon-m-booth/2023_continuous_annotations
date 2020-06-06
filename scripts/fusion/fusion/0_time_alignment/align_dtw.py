f44762316809c462a4fb064d82d40ca6dda61776
Brandon Booth <brandon.m.booth@gmail.com>
2020-06-06 06:55:45 -0700

    Added signal resampling option after time alignment

--- scripts/fusion/fusion/0_time_alignment/align_dtw.py
+++ scripts/fusion/fusion/0_time_alignment/align_dtw.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 import util
 import agreement_metrics as agree
 
-def AlignDTW(input_anno_dir, output_anno_dir, max_warp_seconds=5, show_plots=False):
+def AlignDTW(input_anno_dir, output_anno_dir, max_warp_seconds=5, resample_hz=None, show_plots=False):
    if os.path.isdir(input_anno_dir):
       input_annos = glob.glob(os.path.join(input_anno_dir, '*.csv'))
    elif os.path.isfile(input_anno_dir):
@@ -34,7 +34,7 @@ def AlignDTW(input_anno_dir, output_anno_dir, max_warp_seconds=5, show_plots=Fal
       df = df.set_index(df.columns[0])
       df.index = [datetime.fromtimestamp(t/1000.0, pytz.utc) for t in df.index]
       df = df.resample('100ms').mean().interpolate()
-      df.index = [t.timestamp() for t in df.index]
+      #df.index = [t.timestamp() for t in df.index]
 
       # Use the most agreeable annotation as the reference
       norm_diff_df = util.NormedDiff(df)
@@ -45,19 +45,31 @@ def AlignDTW(input_anno_dir, output_anno_dir, max_warp_seconds=5, show_plots=Fal
       df_ref = df.iloc[:,ref_idx]
       
       dtw_df = agree.DTWReference(df, df_ref, max_warp_distance=max_warp_seconds*10)
+
+      if resample_hz is not None:
+         dtw_df = dtw_df.resample(pd.to_timedelta(str(1.0/resample_hz)+'s')).mean().interpolate()
+         time_scale = 1.0
+      else:
+         time_scale = 10.0 # 100ms -> seconds
+
+      dtw_df.index = [t.timestamp() for t in dtw_df.index]
       dtw_df_cols = dtw_df.columns
-      dtw_df['Time(sec)'] = dtw_df.index*10.0
       dtw_df_cols = dtw_df_cols.insert(0, 'Time(sec)')
+      dtw_df['Time(sec)'] = dtw_df.index*time_scale
       dtw_df = dtw_df[dtw_df_cols]
 
       if show_plots:
          for anno_col in dtw_df.columns[1:]:
-            ax.plot(dtw_df['Time(sec)'], dtw_df[anno_col])
+            ax.plot(dtw_df['Time(sec)'], dtw_df[anno_col], label=anno_col)
+         ax.legend()
          plt.title(os.path.basename(input_anno) + ' DTW annotations')
          plt.show()
 
-      output_file_name = os.path.basename(input_anno)
-      dtw_df.to_csv(os.path.join(output_anno_dir, output_file_name), index=False, header=True)
+      # Write separate files for each annotation
+      for dtw_df_col in dtw_df.columns[1:]:
+         output_file_name = os.path.basename(input_anno).split('.')[0]+'_'+dtw_df_col+'.csv'
+         out_df = dtw_df[['Time(sec)', dtw_df_col]]
+         out_df.to_csv(os.path.join(output_anno_dir, output_file_name), index=False, header=True)
 
    return
 
@@ -66,10 +78,11 @@ if __name__=='__main__':
    parser.add_argument('--input_annos', required=True, help='Path to the (filtered) annotation csv file or folder containing them')
    parser.add_argument('--output_path', required=True, help='Path the folder for aligned annotations')
    parser.add_argument('--max_warp_seconds', default=5, type=int, required=False, help='Maximum number of seconds that the annotations are allowed to be locally distorted')
+   parser.add_argument('--resample_hz', required=False, type=int, help='Sampling rate of output')
    parser.add_argument('--show_plots', required=False, action='store_true', help='Enables display of the aligned annotations')
    try:
       args = parser.parse_args()
    except:
       parser.print_help()
       sys.exit(0)
-   AlignDTW(args.input_annos, args.output_path, args.max_warp_seconds, args.show_plots)
+   AlignDTW(args.input_annos, args.output_path, args.max_warp_seconds, args.resample_hz, args.show_plots)
